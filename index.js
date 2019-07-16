adcdb4a2cc61d2b6ba127409c57b1772fae19139
Rabie TAWIL <rabietawil@gmail.com>
2019-07-16 13:39:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(){
+  const newObject = {...driver};
+  
+}
+function destructivelyUpdateDriverWithKeyAndValue(){
+  
+}
